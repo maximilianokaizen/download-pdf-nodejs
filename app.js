f57943b3252f0c4a26595c79b2a7d224a15bf42b
Maximiliano Rossi <148482605+maximilianokaizen@users.noreply.github.com>
2024-01-24 09:32:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
-const cors = require('cors'); // Importa el módulo cors
+const cors = require('cors'); 
 
 const app = express();
 
-app.use(cors()); // Habilita CORS para todas las rutas
-app.use(express.json()); // Middleware para interpretar el body como JSON
+app.use(cors());
+app.use(express.json()); 
 
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 
 app.post('/pdf', async (req, res) => {
-    const htmlContent = req.body.html; // Se espera recibir un parámetro 'html' con el HTML a convertir a PDF
-    
+    const htmlContent = req.body.html;
     if (!htmlContent) {
         return res.status(400).json({ success: false, message: 'No se proporcionó HTML.' });
     }
-
     try {
         const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
         const page = await browser.newPage();
-
         await page.setContent(htmlContent);
         const pdfBuffer = await page.pdf();
-
         await browser.close();
-
         res.setHeader('Content-Disposition', 'attachment; filename=archivo.pdf');
         res.setHeader('Content-Type', 'application/pdf');
         res.send(pdfBuffer);
-
     } catch (error) {
-        //res.status(500).json({ success: false, message: 'Error al generar el PDF.' });
         res.status(500).json({ success: false, message: error.message, stack: error.stack });
     }
 });
@@ -70,27 +63,16 @@ app.post('/getimage', async (req, res) => {
   try {
       const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
       const page = await browser.newPage();
-
-      // Cargar la URL proporcionada
       await page.goto(url, { waitUntil: 'networkidle2' });
-
-      // Obtener dimensiones de la página
       const { width, height } = await page.evaluate(() => {
           return {
               width: document.documentElement.clientWidth,
               height: document.documentElement.clientHeight,
           };
       });
-
-      // Establecer el tamaño de la página en Puppeteer
       await page.setViewport({ width: 1200, height: 1200 }); // Establecer un tamaño específico
-      //await page.setViewport({ width, height });
-
-      // Capturar la imagen
       const imageBuffer = await page.screenshot({ encoding: 'binary' });
-
       await browser.close();
-
       res.setHeader('Content-Disposition', 'attachment; filename=archivo.png');
       res.setHeader('Content-Type', 'image/png');
       res.send(imageBuffer);
